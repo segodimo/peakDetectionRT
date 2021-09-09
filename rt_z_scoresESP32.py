e91635d59fbddd19f3ade9cabe297440bc458188
segodimo <jsbachb612>
2021-09-09 13:07:55 -0300

    zscore sem numpy

--- rt_z_scoresESP32.py
+++ rt_z_scoresESP32.py
@@ -1,8 +1,25 @@
-import statistics_indepfunc as msf
+import math
 
 def float_list(list):
     floatList = [float(i) for i in list]
     return floatList
+
+def mean(data):
+    if iter(data) is data:
+        data = list(data)
+    return sum(data)/len(data)
+
+def stdev(data, xbar=None):
+    if iter(data) is data:
+        data = list(data)
+    if xbar is None:
+        xbar = sum(data)/len(data)
+    total = total2 = 0
+    for x in data:
+        total += (x - xbar)**2
+        total2 += (x - xbar) 
+    total -= total2**2/len(data)
+    return math.sqrt(total/(len(data) - 1))
 
 class real_time_peak_detection():
     def __init__(self, array, lag, threshold, influence):
@@ -15,8 +32,8 @@ class real_time_peak_detection():
         self.filteredY = float_list(self.y)
         self.avgFilter = [0] * len(self.y)
         self.stdFilter = [0] * len(self.y)
-        self.avgFilter[self.lag - 1] = msf.mean(self.y[0:self.lag])
-        self.stdFilter[self.lag - 1] = msf.stdev(self.y[0:self.lag])
+        self.avgFilter[self.lag - 1] = mean(self.y[0:self.lag])
+        self.stdFilter[self.lag - 1] = stdev(self.y[0:self.lag])
 
     def thresholding_algo(self, new_value):
         self.y.append(new_value)
@@ -29,8 +46,8 @@ class real_time_peak_detection():
             self.filteredY = float_list(self.y)
             self.avgFilter = [0] * len(self.y)
             self.stdFilter = [0] * len(self.y)
-            self.avgFilter[self.lag - 1] = msf.mean(self.y[0:self.lag])
-            self.stdFilter[self.lag - 1] = msf.stdev(self.y[0:self.lag])
+            self.avgFilter[self.lag - 1] = mean(self.y[0:self.lag])
+            self.stdFilter[self.lag - 1] = stdev(self.y[0:self.lag])
             return 0
 
         self.signals += [0]
@@ -45,12 +62,12 @@ class real_time_peak_detection():
                 self.signals[i] = -1
 
             self.filteredY[i] = self.influence * self.y[i] + (1 - self.influence) * self.filteredY[i - 1]
-            self.avgFilter[i] = msf.mean(self.filteredY[(i - self.lag):i])
-            self.stdFilter[i] = msf.stdev(self.filteredY[(i - self.lag):i])
+            self.avgFilter[i] = mean(self.filteredY[(i - self.lag):i])
+            self.stdFilter[i] = stdev(self.filteredY[(i - self.lag):i])
         else:
             self.signals[i] = 0
             self.filteredY[i] = self.y[i]
-            self.avgFilter[i] = msf.mean(self.filteredY[(i - self.lag):i])
-            self.stdFilter[i] = msf.stdev(self.filteredY[(i - self.lag):i])
+            self.avgFilter[i] = mean(self.filteredY[(i - self.lag):i])
+            self.stdFilter[i] = stdev(self.filteredY[(i - self.lag):i])
 
         return self.signals[i]
